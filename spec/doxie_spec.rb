6fc1680723cba297ac0f0ea7f37c8871dcdf6b43
Cristiano Betta <cbetta@box.com>
2020-05-13 11:50:14 +0100

    Bump dependencies. Deprecate Ruby 2.3/2.4

--- spec/doxie_spec.rb
+++ spec/doxie_spec.rb
@@ -38,7 +38,7 @@ describe 'Doxie::Client' do
     it 'should return the result' do
       stub_request(:get, "#{@base_url}/hello.json")
         .to_return(@json_response_body)
-      @client.hello.must_equal(@json_response_object)
+      _(@client.hello).must_equal(@json_response_object)
     end
   end
 
@@ -46,13 +46,13 @@ describe 'Doxie::Client' do
     it 'should return the result' do
       stub_request(:get, "#{@base_url}/hello_extra.json")
         .to_return(@json_response_body)
-      @client.hello_extra.must_equal(@json_response_object)
+      _(@client.hello_extra).must_equal(@json_response_object)
     end
 
     it 'should error for API V2 models, as the method does not exist' do
       @client = Doxie::Client.new(ip: @ip, model: Doxie::API_V2)
-      error = -> { @client.hello_extra }.must_raise(Doxie::Client::Error)
-      error.message.must_match "Method does not exist for this model"
+      error = _(-> { @client.hello_extra }).must_raise(Doxie::Client::Error)
+      _(error.message).must_match "Method does not exist for this model"
     end
   end
 
@@ -60,7 +60,7 @@ describe 'Doxie::Client' do
     it 'should return the result' do
       stub_request(:get, "#{@base_url}/restart.json")
         .to_return(status: 204)
-      @client.restart.must_equal(true)
+      _(@client.restart).must_equal(true)
     end
   end
 
@@ -68,14 +68,14 @@ describe 'Doxie::Client' do
     it 'should return the result' do
       stub_request(:get, "#{@base_url}/scans.json")
         .to_return(@json_response_body)
-      @client.scans.must_equal(@json_response_object)
+      _(@client.scans).must_equal(@json_response_object)
     end
 
     it 'should return an empty array when there are no scans on a V2 model' do
       @client = Doxie::Client.new(ip: @ip, model: Doxie::API_V2)
       stub_request(:get, "#{@base_url_v2}/scans.json")
         .to_return(status: 404)
-      @client.scans.must_equal([])
+      _(@client.scans).must_equal([])
     end
   end
 
@@ -83,14 +83,14 @@ describe 'Doxie::Client' do
     it 'should return the result' do
       stub_request(:get, "#{@base_url}/scans/recent.json")
         .to_return(@json_response_body)
-      @client.recent_scans.must_equal(@json_response_object)
+      _(@client.recent_scans).must_equal(@json_response_object)
     end
 
     it 'should return an empty array when there are no scans on a V2 model' do
       @client = Doxie::Client.new(ip: @ip, model: Doxie::API_V2)
       stub_request(:get, "#{@base_url_v2}/scans/recent.json")
         .to_return(status: 204)
-      @client.recent_scans.must_equal([])
+      _(@client.recent_scans).must_equal([])
     end
   end
 
@@ -98,13 +98,13 @@ describe 'Doxie::Client' do
     it 'should return the result' do
       stub_request(:get, "#{@base_url}/scans/DOXIE/JPEG/IMG_0001.JPG")
         .to_return(@json_response_body)
-      @client.scan('/DOXIE/JPEG/IMG_0001.JPG').must_equal(@json_response_object)
+      _(@client.scan('/DOXIE/JPEG/IMG_0001.JPG')).must_equal(@json_response_object)
     end
 
     it 'should write to file' do
       stub_request(:get, "#{@base_url}/scans/DOXIE/JPEG/IMG_0001.JPG")
         .to_return(@json_response_body)
-      @client.scan('/DOXIE/JPEG/IMG_0001.JPG', 'test.jpg').must_equal(true)
+      _(@client.scan('/DOXIE/JPEG/IMG_0001.JPG', 'test.jpg')).must_equal(true)
     end
   end
 
@@ -112,14 +112,14 @@ describe 'Doxie::Client' do
     it 'should return the result' do
       stub_request(:get, "#{@base_url}/thumbnails/DOXIE/JPEG/IMG_0001.JPG")
         .to_return(@json_response_body)
-      @client.thumbnail('/DOXIE/JPEG/IMG_0001.JPG')
+      _(@client.thumbnail('/DOXIE/JPEG/IMG_0001.JPG'))
              .must_equal(@json_response_object)
     end
 
     it 'should write to file' do
       stub_request(:get, "#{@base_url}/thumbnails/DOXIE/JPEG/IMG_0001.JPG")
         .to_return(@json_response_body)
-      @client.thumbnail('/DOXIE/JPEG/IMG_0001.JPG', 'test.jpg').must_equal(true)
+      _(@client.thumbnail('/DOXIE/JPEG/IMG_0001.JPG', 'test.jpg')).must_equal(true)
     end
   end
 
@@ -127,7 +127,7 @@ describe 'Doxie::Client' do
     it 'should return the result' do
       stub_request(:delete, "#{@base_url}/scans/DOXIE/JPEG/IMG_0001.JPG")
         .to_return(@json_response_body)
-      @client.delete_scan('/DOXIE/JPEG/IMG_0001.JPG')
+      _(@client.delete_scan('/DOXIE/JPEG/IMG_0001.JPG'))
              .must_equal(@json_response_object)
     end
   end
@@ -136,7 +136,7 @@ describe 'Doxie::Client' do
     it 'should return the result' do
       stub_request(:post, "#{@base_url}/scans/delete.json")
         .to_return(status: 204)
-      @client.delete_scans(['/DOXIE/JPEG/IMG_0001.JPG'])
+      _(@client.delete_scans(['/DOXIE/JPEG/IMG_0001.JPG']))
              .must_equal(true)
     end
   end
@@ -144,18 +144,18 @@ describe 'Doxie::Client' do
   it 'raises an authentication error exception if the response code is 401' do
     stub_request(:get, "#{@base_url}/hello.json")
       .to_return(status: 401)
-    proc { @client.hello }.must_raise(Doxie::Client::Error)
+    _(proc { @client.hello }).must_raise(Doxie::Client::Error)
   end
 
   it 'raises a client error exception if the response code is 4xx' do
     stub_request(:get, "#{@base_url}/hello.json")
       .to_return(status: 400)
-    proc { @client.hello }.must_raise(Doxie::Client::Error)
+    _(proc { @client.hello }).must_raise(Doxie::Client::Error)
   end
 
   it 'raises a server error exception if the response code is 5xx' do
     stub_request(:get, "#{@base_url}/hello.json")
       .to_return(status: 500)
-    proc { @client.hello }.must_raise(Doxie::Client::Error)
+    _(proc { @client.hello }).must_raise(Doxie::Client::Error)
   end
 end
